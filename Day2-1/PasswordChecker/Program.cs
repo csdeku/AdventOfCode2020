325845c015beaee62e3c315e84b3562ef834067d
profess <profess@users.noreply.github.com>
2021-01-08 15:53:52 -0500

    2

--- Day2-1/PasswordChecker/Program.cs
+++ Day2-1/PasswordChecker/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace PasswordChecker
 {
@@ -10,6 +13,38 @@ namespace PasswordChecker
     {
         static void Main(string[] args)
         {
+
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+            Thread t = new Thread((ThreadStart)(() =>
+            {
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.InitialDirectory = "c:\\";
+                    openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                    openFileDialog.FilterIndex = 2;
+                    openFileDialog.RestoreDirectory = true;
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        //Get the path of specified file
+                        filePath = openFileDialog.FileName;
+
+                        //Read the contents of the file into a stream
+                        var fileStream = openFileDialog.OpenFile();
+
+                        using (StreamReader reader = new StreamReader(fileStream))
+                        {
+                            fileContent = reader.ReadToEnd();
+                        }
+                    }
+                }
+
+                MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
+            }));
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+            t.Join();            
         }
     }
 }
