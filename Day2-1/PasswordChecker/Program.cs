040158cef8d5c385d6cb419f44966cd33a9454b9
profess <profess@users.noreply.github.com>
2021-01-08 19:00:27 -0500

    3

--- Day2-1/PasswordChecker/Program.cs
+++ Day2-1/PasswordChecker/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -20,7 +21,7 @@ namespace PasswordChecker
             {
                 using (OpenFileDialog openFileDialog = new OpenFileDialog())
                 {
-                    openFileDialog.InitialDirectory = "c:\\";
+                    openFileDialog.InitialDirectory = "D:\\OneDrive\\Shared Folder\\Projects\\AdventOfCode20";
                     openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
                     openFileDialog.FilterIndex = 2;
                     openFileDialog.RestoreDirectory = true;
@@ -39,12 +40,63 @@ namespace PasswordChecker
                         }
                     }
                 }
-
-                MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
             }));
             t.SetApartmentState(ApartmentState.STA);
             t.Start();
-            t.Join();            
+            t.Join();
+            string[] passwordList = fileContent.Split('\n');
+            Console.WriteLine(countValid(passwordList));
+            Console.WriteLine(countValid2(passwordList));
+
         }
+
+        static int countValid(string[] passwordTable)
+        {
+            int totalValid = 0;
+            foreach(string line in passwordTable)
+            {
+                // 3-9 s: bgldhnxsksznscnlnhc
+                var regex = Regex.Match(line, @"([0-9]+)-([0-9]+) ([A-Za-z]): (.+)");
+                if (regex.Groups.Count < 5) continue;
+                int min = int.Parse(regex.Groups[1].Value);
+                int max = int.Parse(regex.Groups[2].Value);
+                char requiredChar = char.Parse(regex.Groups[3].Value);
+                string password = regex.Groups[4].Value;
+                int count = password.Count(f => (f == requiredChar));
+                if (count >= min && count <= max)
+                    totalValid++;
+            }
+
+            return totalValid;
+        }
+
+        static int countValid2(string[] passwordTable)
+        {
+            int totalValid = 0;
+            foreach (string line in passwordTable)
+            {
+                // 3-9 s: bgldhnxsksznscnlnhc
+                var regex = Regex.Match(line, @"([0-9]+)-([0-9]+) ([A-Za-z]): (.+)");
+                if (regex.Groups.Count < 5) continue;
+                int posA = int.Parse(regex.Groups[1].Value) - 1;
+                int posB = int.Parse(regex.Groups[2].Value) - 1;
+                char requiredChar = char.Parse(regex.Groups[3].Value);
+                string password = regex.Groups[4].Value;
+                bool aIncluded = posA < password.Length && password[posA] == requiredChar;
+                bool bIncluded = posB < password.Length && password[posB] == requiredChar;
+                if ((aIncluded && !bIncluded) || (!aIncluded && bIncluded))
+                {
+                    totalValid++;
+                }
+            }
+
+            return totalValid;
+        }
+
+
+
+
+
+
     }
 }
